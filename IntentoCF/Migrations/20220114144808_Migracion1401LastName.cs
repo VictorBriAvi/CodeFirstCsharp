17e15568a0ba0b68f6a6fde1fe5ec9c6b2092463
JuegoSaltarina <victor_15_avila@hotmail.es>
2022-01-14 12:31:46 -0300

    Subiendo CodeFirts

---
+++ IntentoCF/Migrations/20220114144808_Migracion1401LastName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IntentoCF.Migrations
+{
+    public partial class Migracion1401LastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "People");
+        }
+    }
+}
---
+++ IntentoCF/Models/Model.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntentoCF.Models
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = LAPTOP-622ROEGK\SQLEXPRESS; DataBase = CodeFirst; Trusted_Connection=True;");
+        }
+        public DbSet<People> People { get; set; }
+        public DbSet<House> House { get; set; }
+
+
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<House> House { get; set; }
+    }
+
+    public class House
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+
+        public int PeopleId { get; set; }
+        public People People { get; set; }
+    }
+}
